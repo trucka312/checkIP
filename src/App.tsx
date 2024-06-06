e043592e6698cbabeea5978ed80e1d4bd80550aa
trucka312 <trucka312@gmail.com>
2024-06-06 22:57:22 +0700

    u

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ function App() {
         dataToSend
       )
       .then((response) => {
-        console.log("dataa", response.data);
+        console.log("data", response.data);
       })
       .catch((error) => {
         console.error(error);
